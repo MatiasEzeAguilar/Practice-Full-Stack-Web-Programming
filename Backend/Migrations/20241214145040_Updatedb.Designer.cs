91fbfaf29cb2747404d6dd0dcea6211950b9ed30
Matias Ezequiel Aguilar <mattiezeaguila12@gmail.com>
2024-12-14 11:53:33 -0300

    modified:   Backend/AppApi.sql 	modified:   Backend/AppApi.sql-shm 	modified:   Backend/AppApi.sql-wal 	new file:   Backend/Migrations/20241214145040_Updatedb.Designer.cs 	new file:   Backend/Migrations/20241214145040_Updatedb.cs 	modified:   Backend/Migrations/ApplicationContextModelSnapshot.cs

--- Backend/Migrations/20241214145040_Updatedb.Designer.cs
+++ Backend/Migrations/20241214145040_Updatedb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241214145040_Updatedb")]
+    partial class Updatedb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ Backend/Migrations/20241214145040_Updatedb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatedb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdAuthor",
+                table: "Authors",
+                newName: "AuthorIdAuthor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuthorIdAuthor",
+                table: "Authors",
+                newName: "IdAuthor");
+        }
+    }
+}
